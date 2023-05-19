3c81fa5f7179fb9db9a7485a4fd833a68f8fb4b8
hominglesi <creepersheep1010@gmail.com>
2023-05-19 20:09:52 +0200

    Create a wrapper for the XNA Game Class

---
+++ Azalea/GameWrapper.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Azalea;
+
+internal sealed class GameWrapper : Game
+{
+    internal GraphicsDeviceManager Graphics => graphics;
+    private readonly GraphicsDeviceManager graphics;
+
+    public GameWrapper()
+    {
+        graphics = new GraphicsDeviceManager(this);
+        IsMouseVisible = true;
+    }
+
+    internal event Action? OnInitialize;
+    protected override void Initialize()
+    {
+        OnInitialize?.Invoke();
+        base.Initialize();
+    }
+
+    internal event Action? OnLoadContent;
+    protected override void LoadContent()
+    {
+        OnLoadContent?.Invoke();
+    }
+
+    internal event Action<GameTime>? OnUpdate;
+    protected override void Update(GameTime gameTime)
+    {
+        OnUpdate?.Invoke(gameTime);
+        base.Update(gameTime);
+    }
+
+    internal event Action<GameTime>? OnDraw;
+    protected override void Draw(GameTime gameTime)
+    {
+        OnDraw?.Invoke(gameTime);
+        base.Draw(gameTime);
+    }
+}
