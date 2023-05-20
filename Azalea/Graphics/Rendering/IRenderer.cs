e63c05043feb5510c290a0f17c5bae8cd73af427
hominglesi <creepersheep1010@gmail.com>
2023-05-20 03:04:03 +0200

    Add basic Renderer setup

---
+++ Azalea/Graphics/Rendering/IRenderer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azalea.Graphics.Rendering;
+
+public interface IRenderer
+{
+    public Color ClearColor { get; set; }
+
+    internal void Clear();
+}
---
+++ Azalea/Graphics/Rendering/Renderer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Azalea.Graphics.Rendering;
+
+internal abstract class Renderer : IRenderer
+{
+    public Color ClearColor { get; set; }
+
+    public void Clear()
+    {
+        ClearImplementation(ClearColor);
+    }
+
+    protected abstract void ClearImplementation(Color color);
+}
