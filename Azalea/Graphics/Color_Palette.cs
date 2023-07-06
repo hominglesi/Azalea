980693c548d2e5bd7f473d5ad9f088f2302a0b7b
Hominglesi <creepersheep1010@gmail.com>
2023-07-06 23:34:13 +0200

    Add more predefined colors to Color

--- Azalea/Graphics/Color_Palette.cs
+++ Azalea/Graphics/Color_Palette.cs
@@ -7,4 +7,23 @@ public partial struct Color
     public static Color Cornflower => new(100, 149, 237);
 
     #endregion
+
+    #region Colors
+    public static Color Black => new(0, 0, 0);
+    public static Color White => new(255, 255, 255);
+    public static Color Red => new(255, 0, 0);
+    public static Color Lime => new(0, 255, 0);
+    public static Color Blue => new(0, 0, 255);
+    public static Color Yellow => new(255, 255, 0);
+    public static Color Aqua => new(0, 255, 255);
+    public static Color Magenta => new(255, 0, 255);
+    public static Color Silver => new(192, 192, 192);
+    public static Color Gray => new(128, 128, 128);
+    public static Color Maroon => new(128, 0, 0);
+    public static Color Olive => new(128, 128, 0);
+    public static Color Green => new(0, 128, 0);
+    public static Color Purple => new(128, 0, 128);
+    public static Color Teal => new(0, 128, 128);
+    public static Color Navy => new(0, 0, 128);
+    #endregion
 }
