22f8ed89764a1016d21a61266f3633422ad4ab3d
Hominglesi <creepersheep1010@gmail.com>
2023-08-20 00:42:46 +0200

    Add Outline object

---
+++ Azalea/Graphics/Shapes/Outline_DrawNode.cs
@@ -0,0 +1,35 @@
+﻿using Azalea.Graphics.Primitives;
+using Azalea.Graphics.Rendering;
+
+namespace Azalea.Graphics.Shapes;
+
+public partial class Outline
+{
+    internal class OutlineWrapperDrawNode : DrawNode
+    {
+        protected new OutlineWrapper Source => (OutlineWrapper)base.Source;
+
+        protected float Thickness { get; set; }
+        protected Quad ScreenSpaceDrawQuad { get; set; }
+        protected DrawColorInfo ColorInfo { get; set; }
+
+        public OutlineWrapperDrawNode(OutlineWrapper source)
+            : base(source) { }
+
+        public override void ApplyState()
+        {
+            base.ApplyState();
+
+            Thickness = Source.Thickness;
+            ScreenSpaceDrawQuad = Source.WrappedChild.ScreenSpaceDrawQuad;
+            ColorInfo = Source.DrawColorInfo;
+        }
+
+        public override void Draw(IRenderer renderer)
+        {
+            base.Draw(renderer);
+
+            renderer.DrawRectangle(ScreenSpaceDrawQuad, Thickness, ColorInfo);
+        }
+    }
+}
