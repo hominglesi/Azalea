573ae15a459687f6f7d184c440e2e2f0260d00d7
Hominglesi <creepersheep1010@gmail.com>
2023-09-10 16:09:47 +0200

    Remove all references to MonoGame
    Closes #8

--- Azalea/Graphics/ColorExtentions.cs
+++ Azalea/Graphics/ColorExtentions.cs
@@ -4,9 +4,7 @@ namespace Azalea.Graphics;
 
 public static class ColorExtentions
 {
-    internal static Microsoft.Xna.Framework.Color ToXNAColor(this Color color)
-        => new(color.R, color.G, color.B, color.A);
 
-    internal static Vector4 ToVector4(this Color color)
-        => new(color.RNormalized, color.GNormalized, color.BNormalized, color.ANormalized);
+	internal static Vector4 ToVector4(this Color color)
+		=> new(color.RNormalized, color.GNormalized, color.BNormalized, color.ANormalized);
 }
--- Azalea/Host.cs
+++ Azalea/Host.cs
@@ -1,23 +1,21 @@
 ﻿using Azalea.Platform;
 using Azalea.Platform.Silk;
 using Azalea.Platform.Veldrid;
-using Azalea.Platform.XNA;
 using System;
 
 namespace Azalea;
 
 public static class Host
 {
-    public static GameHost CreateHost(HostPreferences preferences = default)
-    {
-        return preferences.Type switch
-        {
-            HostType.XNA => new XNAGameHost(preferences),
-            HostType.Silk => new SilkGameHost(preferences),
-            HostType.Veldrid => new VeldridGameHost(preferences),
-            _ => throw new Exception($"Host of type {preferences.Type} does not exist")
-        };
-    }
+	public static GameHost CreateHost(HostPreferences preferences = default)
+	{
+		return preferences.Type switch
+		{
+			HostType.Silk => new SilkGameHost(preferences),
+			HostType.Veldrid => new VeldridGameHost(preferences),
+			_ => throw new Exception($"Host of type {preferences.Type} does not exist")
+		};
+	}
 }
 
 
