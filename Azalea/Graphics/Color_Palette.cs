1e25b7070fc3393a07372378f7d03fda0a1fedd5
Hominglesi <creepersheep1010@gmail.com>
2023-09-14 23:42:31 +0200

    Add new colors

--- Azalea/Graphics/Color_Palette.cs
+++ Azalea/Graphics/Color_Palette.cs
@@ -5,25 +5,36 @@ public partial struct Color
 	#region Flowers
 	public static Color Azalea => new(201, 98, 143);
 	public static Color Cornflower => new(100, 149, 237);
-
+	public static Color Lavender => new(230, 230, 250);
+	public static Color Orchid => new(218, 112, 214);
 	#endregion
 
 	#region Colors
-	public static Color Black => new(0, 0, 0);
-	public static Color White => new(255, 255, 255);
-	public static Color Red => new(255, 0, 0);
-	public static Color Lime => new(0, 255, 0);
-	public static Color Blue => new(0, 0, 255);
-	public static Color Yellow => new(255, 255, 0);
 	public static Color Aqua => new(0, 255, 255);
-	public static Color Magenta => new(255, 0, 255);
-	public static Color Silver => new(192, 192, 192);
+	public static Color Beige => new(245, 245, 220);
+	public static Color Black => new(0, 0, 0);
+	public static Color Blue => new(0, 0, 255);
+	public static Color Brown => new(165, 42, 42);
+	public static Color Crimson => new(220, 20, 60);
+	public static Color Cyan => new(0, 255, 255);
+	public static Color Gold => new(255, 215, 0);
 	public static Color Gray => new(128, 128, 128);
-	public static Color Maroon => new(128, 0, 0);
-	public static Color Olive => new(128, 128, 0);
 	public static Color Green => new(0, 128, 0);
-	public static Color Purple => new(128, 0, 128);
-	public static Color Teal => new(0, 128, 128);
+	public static Color Indigo => new(75, 0, 130);
+	public static Color Lime => new(0, 255, 0);
+	public static Color Magenta => new(255, 0, 255);
+	public static Color Maroon => new(128, 0, 0);
 	public static Color Navy => new(0, 0, 128);
+	public static Color Olive => new(128, 128, 0);
+	public static Color Orange => new(255, 165, 0);
+	public static Color Pink => new(255, 192, 203);
+	public static Color Purple => new(128, 0, 128);
+	public static Color Red => new(255, 0, 0);
+	public static Color Silver => new(192, 192, 192);
+	public static Color Teal => new(0, 128, 128);
+	public static Color Turquoise => new(64, 224, 208);
+	public static Color Violet => new(238, 130, 238);
+	public static Color Yellow => new(255, 255, 0);
+	public static Color White => new(255, 255, 255);
 	#endregion
 }
