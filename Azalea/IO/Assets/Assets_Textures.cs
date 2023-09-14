af8bf3ee2a65c0331d989d3b87d691c780cc82f4
Hominglesi <creepersheep1010@gmail.com>
2023-09-14 02:51:48 +0200

    Add MissingTexture property

--- Azalea/IO/Assets/Assets_Textures.cs
+++ Azalea/IO/Assets/Assets_Textures.cs
@@ -7,10 +7,11 @@ namespace Azalea.IO.Assets;
 public static partial class Assets
 {
 	private const string missing_texture_path = "missing-texture.png";
+	public static Texture MissingTexture => GetTexture(missing_texture_path);
 
 	public static Texture GetTexture(string path)
 	{
-		return Textures.Get(path) ?? Textures.Get(missing_texture_path) ?? throw new Exception("Missing texture could not be found.");
+		return Textures.Get(path) ?? MissingTexture ?? throw new Exception("Texture could not be found.");
 	}
 
 	public static Stream? GetTextureStream(string path) => Textures.GetStream(path);
