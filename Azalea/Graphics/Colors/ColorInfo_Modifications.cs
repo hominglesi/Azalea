3f0397edd1e2b6de02b98a6f1fe21cee6c2498bc
Hominglesi <38411358+Hominglesi@users.noreply.github.com>
2023-09-24 01:43:35 +0200

    Add more color modifications

--- Azalea/Graphics/Colors/ColorInfo_Modifications.cs
+++ Azalea/Graphics/Colors/ColorInfo_Modifications.cs
@@ -1,6 +1,51 @@
 ﻿namespace Azalea.Graphics.Colors;
 public partial struct ColorInfo
 {
+	public readonly ColorInfo ModifyHue(float value)
+	{
+		if (HasSingleColor)
+		{
+			var color = SingleColor;
+			color.Hue += value;
+			return SolidColor(color);
+		}
+		else
+		{
+			var topLeft = TopLeft;
+			topLeft.Hue += value;
+			var bottomLeft = BottomLeft;
+			bottomLeft.Hue += value;
+			var bottomRight = BottomRight;
+			bottomRight.Hue += value;
+			var topRight = TopRight;
+			topRight.Hue += value;
+
+			return new ColorInfo(topLeft, bottomLeft, bottomRight, topRight);
+		}
+	}
+	public readonly ColorInfo ModifySaturation(float value)
+	{
+		if (HasSingleColor)
+		{
+			var color = SingleColor;
+			color.Saturation += value;
+			return SolidColor(color);
+		}
+		else
+		{
+			var topLeft = TopLeft;
+			topLeft.Saturation += value;
+			var bottomLeft = BottomLeft;
+			bottomLeft.Saturation += value;
+			var bottomRight = BottomRight;
+			bottomRight.Saturation += value;
+			var topRight = TopRight;
+			topRight.Saturation += value;
+
+			return new ColorInfo(topLeft, bottomLeft, bottomRight, topRight);
+		}
+	}
+
 	public readonly ColorInfo ModifyLuminance(float value)
 	{
 		if (HasSingleColor)
