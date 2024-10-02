6d14faddebc01a01de4ea2cfd570cfe3c802f2cd
Hominglesi <creepersheep1010@gmail.com>
2024-10-02 23:50:24 +0200

    Add static files from package

--- Azalea.VisualTests.Web/wwwroot/exports.js
+++
@@ -1,5 +0,0 @@
-﻿var exports;
-
-// WebEvents
-const UpdateClientSize = (width, heigth) => exports.Azalea.Web.WebEvents.UpdateClientSize(width, heigth);
-const InvokeAnimationFrameRequested = (framerate) => exports.Azalea.Web.WebEvents.InvokeAnimationFrameRequested();
